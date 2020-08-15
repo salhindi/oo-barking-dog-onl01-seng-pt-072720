aa82eaecec819660dc78423bb3e6cd145650d3fe
Sara A <calisaraalhindi@gmail.com>
2020-08-15 19:46:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 # Your code goes here!
 class Dog
-  that_dogs_name
+  def name= (dogs_name)
+    @this_dogs_name =dogs_name
+    end
+    
+    def name 
+      @this_dogs_name
+    end
+  end
+  
+  fido = Dog.new
+  fido.name = "Fido"
+  
+  puts Fido.name
+
+
+  
